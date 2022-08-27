c44712c629b3a6b46d97c0ce11f3e9401ffd6397
LazarevaL <l.lazareva@edu.misis.ru>
2022-08-27 19:04:22 +0300

    done

--- sql_querys.py
+++ sql_querys.py
@@ -1,6 +1,6 @@
 # INFO
 # Вывести топ 5 коротких по длительности перелетов.  Duration - разница между scheduled_arrival и scheduled_departure.
-# В ответе должно быть 2 колонки [flight_no, duration]
+# В ответе должно быть 2 колонк [flight_no, duration]
 TASK_1_QUERY = """
 SELECT flight_no, (scheduled_arrival - scheduled_departure) as duration
 FROM flights 
